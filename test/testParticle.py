f0010539689eacbde4866a5b09a11877d4bbf98e
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-09 18:28:06 +0800

    Update testParticle.py

--- test/testParticle.py
+++ test/testParticle.py
@@ -3,6 +3,12 @@ import simtk.openmm.app as app
 import numpy as np
 import simtk.unit as u
 import openmmcosacc
+import sys
+
+if len(sys.argv) > 1:
+    platformName = sys.argv[1]
+else:
+    platformName = "Reference"
 
 neforce = openmmcosacc.CosAccForce(0.25 * u.nanometer / u.picosecond ** 2)
 
@@ -24,7 +30,7 @@ system.setDefaultPeriodicBoxVectors(cell[:,0], cell[:,1], cell[:,2])
 system.addForce(neforce)
 
 integ = mm.VerletIntegrator(0.5 * u.femtosecond)
-platform = mm.Platform.getPlatformByName("Reference")
+platform = mm.Platform.getPlatformByName(platformName)
 ctx = mm.Context(system, integ, platform)
 ctx.setPositions(pos)
 
