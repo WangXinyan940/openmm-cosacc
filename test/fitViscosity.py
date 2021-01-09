8e87b960fa2474f5f8068ae267a2db4ea55e0397
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-09 21:46:03 +0800

    Update fitViscosity.py

--- test/fitViscosity.py
+++ test/fitViscosity.py
@@ -12,10 +12,11 @@ xbins = np.linspace(0.0, 2.4, 41)
 nbins = np.zeros((40,))
 totbins = np.zeros((40,))
 
+padd = posz // (xbins[1] - xbins[0])
 for ii in range(posz.shape[0]):
     if ii%10000 ==0:
         print("%4.2f/100"%(100.0*ii/posz.shape[0]))
-    iadd = posz[ii] // (xbins[1] - xbins[0])
+    iadd = int(padd[ii])
     if iadd < 40:
         nbins[iadd] += 1
         totbins[iadd] += velx[ii]
