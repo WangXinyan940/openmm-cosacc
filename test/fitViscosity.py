2ed57fd13e4887c28eb95a90dfa0ae2ac665ed2f
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-09 21:44:51 +0800

    Update fitViscosity.py

--- test/fitViscosity.py
+++ test/fitViscosity.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 with open("posz.txt", "r") as f:
     posz = np.array([[float(j) for j in i.strip().split()] for i in f]).ravel()
+    posz = posz % 2.4
 
 with open("velx.txt", "r") as f:
     velx = np.array([[float(j) for j in i.strip().split()] for i in f]).ravel()
@@ -14,12 +15,10 @@ totbins = np.zeros((40,))
 for ii in range(posz.shape[0]):
     if ii%10000 ==0:
         print("%4.2f/100"%(100.0*ii/posz.shape[0]))
-    tempz = posz[ii] % 2.4
-    for n in range(40):
-        iadd = tempz // (xbins[1] - xbins[0])
-        if iadd < 40:
-            nbins[iadd] += 1
-            totbins[iadd] += velx[ii]
+    iadd = posz[ii] // (xbins[1] - xbins[0])
+    if iadd < 40:
+        nbins[iadd] += 1
+        totbins[iadd] += velx[ii]
 
 xaxis = (xbins[:-1] + xbins[1:]) / 2
 plt.plot(xaxis, totbins / nbins)
