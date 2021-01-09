a5c18a7d500645920b1d701cf9764fc709cac0c4
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-09 18:25:18 +0800

    Update testParticle.py

--- test/testParticle.py
+++ test/testParticle.py
@@ -5,3 +5,33 @@ import simtk.unit as u
 import openmmcosacc
 
 neforce = openmmcosacc.CosAccForce(0.25 * u.nanometer / u.picosecond ** 2)
+
+system = mm.System()
+for _ in range(8):
+    system.addParticle(8.0)
+
+cell = np.array([
+    [8.0, 0.0, 0.0],
+    [0.0, 8.0, 0.0],
+    [0.0, 0.0, 8.0]
+]) * u.nanometer
+
+pos = np.zeros((8,3))
+for i in range(8):
+    pos[i,2] = i
+
+system.setDefaultPeriodicBoxVectors(cell[:,0], cell[:,1], cell[:,2])
+system.addForce(neforce)
+
+integ = mm.VerletIntegrator(0.5 * u.femtosecond)
+platform = mm.Platform.getPlatformByName("Reference")
+ctx = mm.Context(system, integ, platform)
+
+for i in range(100):
+    integ.step(2)
+    state = ctx.getState(getPositions=True, getVelocities=True)
+    print("Step:", i)
+    print("Pos:")
+    print(state.getPositions(asNumpy=True))
+    print("Vel:")
+    print(state.getVelocities(asNumpy=True))
