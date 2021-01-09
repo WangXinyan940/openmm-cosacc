7d1920b0909973ab069b98546157d518ec2c5899
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-09 19:06:01 +0800

    Update cosAccForce.cu

--- platforms/cuda/src/kernels/cosAccForce.cu
+++ platforms/cuda/src/kernels/cosAccForce.cu
@@ -1,5 +1,11 @@
 extern "C" __global__ 
-void addForces(const FORCES_TYPE* __restrict__ massvec, const real4* __restrict__ posq, long long* __restrict__ forceBuffers, int* __restrict__ atomIndex, FORCES_TYPE A, int numAtoms, int paddedNumAtoms) {
+void addForces(const FORCES_TYPE*   __restrict__   massvec, 
+               const real4*         __restrict__   posq, 
+               long long*           __restrict__   forceBuffers, 
+               int*                 __restrict__   atomIndex, 
+               FORCES_TYPE                         A, 
+               int                                 numAtoms, 
+               int                                 paddedNumAtoms) {
     //for (int atom = blockIdx.x*blockDim.x+threadIdx.x; atom < numAtoms; atom += blockDim.x*gridDim.x) {
     //    int index = atomIndex[atom];
     //    FORCES_TYPE addfrc = A * COS(posq[index].z*2PIONELZ) * massvec[index];
