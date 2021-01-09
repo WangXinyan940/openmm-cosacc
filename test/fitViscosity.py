e26110ba72d3b94004f32f4be0c633e4597de189
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-09 21:41:42 +0800

    Update fitViscosity.py

--- test/fitViscosity.py
+++ test/fitViscosity.py
@@ -14,17 +14,12 @@ totbins = np.zeros((40,))
 for ii in range(posz.shape[0]):
     if ii%10000 ==0:
         print("%4.2f/100"%(100.0*ii/posz.shape[0]))
-    tempz = posz[ii]
-    while tempz < 0.0 or tempz > 2.4:
-        if tempz < 0.0:
-            tempz += 2.4
-        elif tempz > 2.4:
-            tempz -= 2.4
+    tempz = posz[ii] % 2.4
     for n in range(40):
-        if xbins[n] < tempz and xbins[n+1] > tempz:
-            nbins[n] += 1
-            totbins[n] += velx[ii]
-            break
+        iadd = tempz % (xbins[1] - xbins[0])
+        if iadd < 40:
+            nbins[iadd] += 1
+            totbins[iadd] += velx[ii]
 
 xaxis = (xbins[:-1] + xbins[1:]) / 2
 plt.plot(xaxis, totbins / nbins)
