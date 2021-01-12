908466035417662121944b0781dcd8d516615a5b
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-12 19:52:07 +0800

    Revert "Update cosAccForce.cu"
    This reverts commit 3f774d314e6bc20f08cf65d9ffe6bdf9264e1b5a.

--- platforms/cuda/src/kernels/cosAccForce.cu
+++ platforms/cuda/src/kernels/cosAccForce.cu
@@ -8,7 +8,7 @@ void addVels(const real4*    __restrict__   posq,
    for (int atom = blockIdx.x*blockDim.x+threadIdx.x; atom < numAtoms; atom += blockDim.x*gridDim.x) {
        int index = atomIndex[atom];
        double addvel = A * COS(posq[index].z*TWOPIOVERLZ);
-       velm[atom].x += addvel;
+       velm[atom][0] += addvel;
    }
 }
 
