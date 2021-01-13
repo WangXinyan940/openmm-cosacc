387f771eec4ae54493d7dbfc9b51c0bcb5a8656d
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-01-13 17:57:54 +0800

    Revert "update"
    This reverts commit b99d16b4510fbad1768909f7a04e72c89c60beb5.

--- platforms/cuda/src/kernels/cosAccForce.cu
+++ platforms/cuda/src/kernels/cosAccForce.cu
@@ -2,11 +2,13 @@ extern "C" __global__
 void addForces(const real*     __restrict__   massvec, 
                const real4*    __restrict__   posq, 
                long long*      __restrict__   forceBuffers, 
+               int*            __restrict__   atomIndex, 
                double                         A, 
                int                            numAtoms, 
                int                            paddedNumAtoms) {
    for (int atom = blockIdx.x*blockDim.x+threadIdx.x; atom < numAtoms; atom += blockDim.x*gridDim.x) {
-       real addfrc = A * COS(posq[atom].z*TWOPIOVERLZ) * massvec[atom];
+       int index = atomIndex[atom];
+       real addfrc = A * COS(posq[index].z*TWOPIOVERLZ) * massvec[index];
        forceBuffers[atom] += (long long) (addfrc*0x100000000);
    }
 }
